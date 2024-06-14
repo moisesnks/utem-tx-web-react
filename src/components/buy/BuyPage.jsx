10f7c7d0b074ccc23fee20c8289b76e1676aecc2
Enzogus <etroncoso@utem.cl>
2024-06-14 04:57:52 -0400

    buypage maomeno

--- src/components/buy/BuyPage.jsx
+++ src/components/buy/BuyPage.jsx
@@ -4,43 +4,94 @@ import CurrencyInput from './CurrencyInput.jsx';
 const BuyPage = () => {
     const [amount, setAmount] = useState('');
     const [convertedAmount, setConvertedAmount] = useState('');
-    const [selectedCurrency, setSelectedCurrency] = useState('BTC');
+    const [selectedCurrency1, setSelectedCurrency1] = useState('CLP');
+    const [selectedCurrency2, setSelectedCurrency2] = useState('BTC');
+    const [mode, setMode] = useState('buy'); // Estado para controlar el modo: 'buy' o 'sell'
 
     const handleAmountChange = (value) => {
         setAmount(value);
-        setConvertedAmount(value*2);
-
+        setConvertedAmount(value * 2);
     };
 
-    const handleCurrencyChange = (currency) => {
-        setSelectedCurrency(currency);
+    const handleCurrencyChange1 = (currency) => {
+        setSelectedCurrency1(currency);
     };
+
+    const handleCurrencyChange2 = (currency) => {
+        setSelectedCurrency2(currency);
+    };
+
     const handleConvertChange = (value) => {
-        setConvertedAmount(value*2);
-    }
+        setConvertedAmount(value * 2);
+    };
+
+    const toggleMode = () => {
+        setMode(mode === 'buy' ? 'sell' : 'buy');
+    };
 
     return (
-        <div>
-            <h1>Buy Page</h1>
-            <CurrencyInput
-                label="Gastar"
-                value={amount}
-                onChange={handleAmountChange}
-                currencies={['BTC', 'ETH']}
-                selectedCurrency={selectedCurrency}
-                onCurrencyChange={handleCurrencyChange}
-                balance={{ BTC: 100, ETH: 200 }} // Example balance data
-            />
-            <CurrencyInput 
-                label="Recibir"
-                value={convertedAmount}
-                onChange={handleConvertChange}
-                currencies={['BTC', 'ETH']}
-                selectedCurrency={selectedCurrency}
-                onCurrencyChange={handleCurrencyChange}
-                balance={{ BTC: 100, ETH: 200 }} // Example balance data
-                isEditable={false}
-            />
+        <div className="container mx-auto p-4">
+        <h1 className="absolute left-3 text-4xl font-bold mb-4">
+            {mode === 'buy' ? 'Compra de Crypto' : 'Venta de Crypto'}
+        </h1>
+            <div className="absolute top-5 right-5 min-h-screen flex items-center justify-end">
+                <div className="p-4 border border-gray-700 rounded-lg bg-gray-800 relative">
+                    <button
+                        onClick={toggleMode}
+                        className="absolute top-0 right-0 mt-2 mr-2 px-2 py-2 bg-gray-700 border border-gray-700 text-white rounded-lg focus:outline-none"
+                    >
+                        {mode === 'buy' ? 'Vender' : 'Comprar'}
+                    </button>
+                    <h2 className="text-xl font-bold text-white mb-4">{mode === 'buy' ? 'Compra' : 'Venta'}</h2>
+                    <div className="flex flex-col gap-4">
+                        {mode === 'buy' ? (
+                            <>
+                                <CurrencyInput
+                                    label="Gastar"
+                                    value={amount}
+                                    onChange={handleAmountChange}
+                                    currencies={['CLP', 'USD']}
+                                    selectedCurrency={selectedCurrency1}
+                                    onCurrencyChange={handleCurrencyChange1}
+                                    balance={{ BTC: 100, ETH: 200 }}
+                                />
+                                <CurrencyInput
+                                    label="Recibir"
+                                    value={convertedAmount}
+                                    onChange={handleConvertChange}
+                                    currencies={['BTC', 'ETH']}
+                                    selectedCurrency={selectedCurrency2}
+                                    onCurrencyChange={handleCurrencyChange2}
+                                    balance={{ BTC: 100, ETH: 200 }}
+                                    isEditable={false}
+                                />
+                            </>
+                        ) : (
+                            <>
+                                <CurrencyInput
+                                    label="Gastar"
+                                    value={amount}
+                                    onChange={handleAmountChange}
+                                    currencies={['BTC', 'ETH']}
+                                    selectedCurrency={selectedCurrency2}
+                                    onCurrencyChange={handleCurrencyChange2}
+                                    balance={{ BTC: 100, ETH: 200 }}
+                                />
+                                <CurrencyInput
+                                    label="Recibir"
+                                    value={convertedAmount}
+                                    onChange={handleConvertChange}
+                                    currencies={['BTC', 'ETH','PEPE']}
+                                    selectedCurrency={selectedCurrency1}
+                                    onCurrencyChange={handleCurrencyChange1}
+                                    balance={{ BTC: 100, ETH: 200 }}
+                                    isEditable={false}
+                                />
+                            </>
+                        )}
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
--- src/components/buy/CurrencyInput.jsx
+++ src/components/buy/CurrencyInput.jsx
@@ -6,26 +6,26 @@ const CurrencyInput = ({ isEditable=true,label, value, onChange, currencies, sel
 
   return (
     <div className="mb-4">
-      <label className="block text-gray-700">{label}</label>
+      <label className="block text-gray-500 ">{label}</label>
       <div className="flex items-center space-x-2 mt-2">
         <input
           type="number"
           value={value}
           onChange={(e) => onChange(e.target.value)}
-          className="flex-1 px-4 py-2 border rounded"
+          className="flex-1 px-4 py-2 border rounded border-gray-700"
           readOnly={!isEditable}
         />
         <select
           value={selectedCurrency}
           onChange={(e) => onCurrencyChange(e.target.value)}
-          className="px-4 py-2 border rounded"
+          className="px-4 py-2 border rounded border-gray-700"
         >
           {currencies.map(currency => (
             <option key={currency} value={currency}>{currency}</option>
           ))}
         </select>
       </div>
-      <small className="block mt-1 text-gray-500">Saldo: {balance[selectedCurrency]} {selectedCurrency}</small>
+      <small className="block mt-1 text-gray-600">Saldo: {balance[selectedCurrency]} {selectedCurrency}</small>
 
     </div>
   );
