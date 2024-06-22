307ea5e72b6c2cd99cc5da4ffce6388ec1b9a44e
moisesnks <mleiva@utem.cl>
2024-06-21 21:17:54 -0400

    viteconfig

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/',
   plugins: [react()],
-  optimizeDeps: {
-    include: ['chart.js', 'chartjs-chart-financial', 'chartjs-plugin-zoom']
-  }
-});
+})
